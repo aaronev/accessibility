4ecc8d405e068a91761e115a4835ef60a5ce043a
Aaron Villanueva <aarone.villanueva@gmail.com>
2019-02-11 18:25:08 -0800

    completes simple accessible file selector

---
+++ file-selector/index.js
@@ -0,0 +1,16 @@
+document.querySelector('form')
+.addEventListener('submit', function(e) {
+  e.preventDefault();
+});
+
+document.querySelector('button')
+.addEventListener('click', function(e) {
+  e.target.parentNode.children[0].click();
+})
+
+document.querySelector('input[type=\'file\']')
+.addEventListener('change', function(e) {
+  input = e.target;
+  console.log(input.parentNode.children[2]);
+  input.parentNode.children[2].value = input.value.split('\\').pop();
+})
